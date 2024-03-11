8141ce275709828ae6b2d80c1cb6dd4b5bffc2e0
soumyajindal07 <54813070+soumyajindal07@users.noreply.github.com>
2024-03-11 14:13:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,9 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"])
+    allow_headers=["*"]
+    
+)
 
 @app.get("/isTokenizerVocabTrainingDataAvailable")
 def isTokenizerVocabTrainingDataAvailable():
